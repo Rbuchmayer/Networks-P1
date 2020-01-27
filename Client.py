cc2e34b0612e0e99d0d865b68ff08b2c5347b9f2
Rbuchmayer <rbuchmayer@yahoo.com>
2020-01-26 16:00:20 -0800

    Add files via upload

--- Client.py
+++ Client.py
@@ -3,27 +3,27 @@ import struct
 
 # Stage a
 
-ATTU_IP = socket.gethostbyname('localhost')
+IP = 'localhost'
 UDP_PORT = 12235
 MESSAGE = struct.pack('!IIHH11sx', 12, 0, 1, 495, bytes('hello world', 'ascii'))
 
 # send UDP to server
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (ATTU_IP, UDP_PORT))
+sock.settimeout(0.5)
+sock.sendto(MESSAGE, (IP, UDP_PORT))
 
 # receive UDP from server
 data_a, addr = sock.recvfrom(28) # buffer size is 28 bytes
 data_a = struct.unpack('!IIHHIIII', data_a)
-print("received message: ", data_a)
 
 # Stage b
-sock.settimeout(0.5)
 num = data_a[4]
 len = data_a[5]
 udp_port = data_a[6]
 secret_a = data_a[7]
-
+print(num)
 for i in range(0, num):
+    print(i)
     pad = ''
     for _ in range(0, len + (4 - (len % 4))):
         pad += 'x'
@@ -31,10 +31,9 @@ for i in range(0, num):
 
     while True:
         try:
-            print(i)
-            sock.sendto(MESSAGE, (ATTU_IP, udp_port))
+            sock.sendto(MESSAGE, (IP, udp_port))
+            print('waiting to recieve')
             data_ack, addr = sock.recvfrom(16)
-            print(struct.unpack('!IIHHI', data_ack))
             break
         except:
             continue
@@ -49,11 +48,9 @@ sock.close()
 
 # Stage C
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.connect((ATTU_IP, tcp_port))
-print('!!')
+tcp_socket.connect((IP, tcp_port))
 data_c2, addr = tcp_socket.recvfrom(28)
 data_c2 = struct.unpack('!IIHHIIIBxxx', data_c2)
-
 num_2 = data_c2[4]
 len_2 = data_c2[5]
 secret_c = data_c2[6]
@@ -70,14 +67,14 @@ for i in range(0, num_2):
         payload += chr(c)
 
     MESSAGE = struct.pack('!IIHH' + str(len_2) + 's' + pad, len_2, secret_c, 1, 495, bytes(payload, 'ascii'))
-    tcp_socket.sendto(MESSAGE, (ATTU_IP, tcp_port))
+    tcp_socket.sendto(MESSAGE, (IP, tcp_port))
 
 # d2
 data_d2, addr_d2 = tcp_socket.recvfrom(16)
 data_d2 = struct.unpack('!IIHHI', data_d2)
 secret_d = data_d2[4]
 tcp_socket.close()
-print('Done')
-
-
-
+print(secret_a)
+print(secret_b)
+print(secret_c)
+print(secret_d)
--- Server.py
+++ Server.py
@@ -1,9 +1,10 @@
 import socketserver
 import struct
 import random
+import threading
 
 state = {}
-state['next_to_ack'] = 0
+tcp_to_id = {}
 
 
 class MyStageAHandler(socketserver.BaseRequestHandler):
@@ -11,6 +12,7 @@ class MyStageAHandler(socketserver.BaseRequestHandler):
         data = self.request[0].strip()
         socket = self.request[1]
         try:
+            socket.settimeout(3)
             data = struct.unpack('!IIHH11sx', data)
             if data[0] != len(data[4]) + 1 or data[1] != 0 or data[2] != 1 or data[4].decode('ascii') != 'hello world':
                 socket.close()
@@ -19,58 +21,57 @@ class MyStageAHandler(socketserver.BaseRequestHandler):
             len_1 = random.randint(1, 10)
             udp_port = random.randint(40000, 50000)
             secret_a = random.randint(1, 100)
-            state[data[3]] = (num, len_1, udp_port, secret_a)
+            state[data[3]] = {'num_1' : num, 'len_1' : len_1, 'udp_port' : udp_port, 'secret_a' : secret_a, 'expected_packet' : 0}
             MESSAGE = struct.pack('!IIHHIIII', 16, data[1], 2, data[3], num, len_1, udp_port, secret_a)
             socket.sendto(MESSAGE, self.client_address)
-            socket.close()
-
-            state['num_packets'] = num
             stage_2(udp_port)
 
         except Exception as ex:
-            print(type(ex))
-            print(ex)
             socket.close()
             return
 
 
 class MyStageBHandler(socketserver.BaseRequestHandler):
-    current_packet_id = 0
+
+
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
         try:
+            socket.settimeout(3)
             # decide to acknowledge
             rand = random.random()
             if rand < 0.5:
                 header = struct.unpack('!IIHH', data[:12])
                 payload_len = header[0]
                 psecret = header[1]
-                secret_a = state[header[3]][3]
+                secret_a = state[header[3]]['secret_a']
                 pad = ''
+
                 for i in range(0, (payload_len - 4) + (4 - (payload_len % 4))):
                     pad += 'x'
 
                 payload_plus_id = struct.unpack('!I' + pad, data[12:])
+                expected_packet = state[header[3]]['expected_packet']
+                expectecd_data = [0 for _ in range(len(data[16:]))]
+                unpacked_data = [b for b in data[16:]]
 
-                if payload_plus_id[0] == state['next_to_ack'] and psecret == secret_a \
-                        and len(data[12:]) == header[0] + (4 - (header[0] % 4)):
+                if payload_plus_id[0] == expected_packet and psecret == secret_a \
+                        and len(data[12:]) == header[0] + (4 - (header[0] % 4)) and expectecd_data == unpacked_data:
 
                     MESSAGE = struct.pack('!IIHHI', 4, psecret, 2, header[3], payload_plus_id[0])
                     socket.sendto(MESSAGE, self.client_address)
-                    state['next_to_ack'] = state['next_to_ack'] + 1
-
+                    state[header[3]]['expected_packet'] += 1
                     # check if last packet
-                    if state['next_to_ack'] == state['num_packets']:
+                    if state[header[3]]['expected_packet'] == state[header[3]]['num_1']:
                         tcp_port = random.randint(40000, 50000)
                         secret_b = random.randint(1, 100)
-                        state[header[3]] = (tcp_port, secret_b)
-
+                        state[header[3]]['secret_b'] = secret_b
+                        tcp_to_id[tcp_port] = header[3]
                         # send next stage info
                         MESSAGE = struct.pack('!IIHHII', 8, psecret, 2, header[3], tcp_port, secret_b)
                         socket.sendto(MESSAGE, self.client_address)
-                        socket.close()
-
+                        stage_3(tcp_port)
 
         except:
             socket.close()
@@ -78,33 +79,54 @@ class MyStageBHandler(socketserver.BaseRequestHandler):
 
 class MyStageCHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
         try:
+            self.request.settimeout(3)
             secret_c = random.randint(1, 100)
-            secret_b = random.randint(1, 100)
-            student_id = 5
+            student_id = tcp_to_id[self.request.getsockname()[1]]
+            secret_b = state[student_id]['secret_b']
+
             num_2 = random.randint(1, 10)
             len_2 = random.randint(1, 10)
-            c = chr(random.randint(1, 100))
+            c = random.randint(1, 100)
             MESSAGE = struct.pack('!IIHHIIIBxxx', 13, secret_b, 2, student_id, num_2, len_2, secret_c, c)
-            socket.sendto(MESSAGE, self.client_address)
-        except:
-            socket.close()
+            self.request.sendto(MESSAGE, self.client_address)
+            pad = ''
+            for _ in range(0, (4 - (len_2 % 4))):
+                pad += 'x'
+            buffer = 12 + len_2 + len(pad)
+            for _ in range(0, num_2):
+                data = self.request.recv(buffer).strip()
+                header = struct.unpack('!IIHH' + str(len_2) + 's' + pad, data)
+                expected_data = [c for _ in range(len_2)]
+                unpacked_data = [b for b in header[4]]
+                if header[1] != secret_c or len(header[4]) != header[0] or unpacked_data != expected_data:
+                    self.request.close()
+            secret_d = random.randint(1, 100)
+            MESSAGE = struct.pack('!IIHHI', 4, secret_c, 2, student_id, secret_d)
+            self.request.sendto(MESSAGE, self.client_address)
+            del state[student_id]
+        except Exception as e:
+            print(e)
+            self.request.close()
 
 
 def stage_2(port):
-    server_2 = socketserver.UDPServer((HOST, port), MyStageBHandler)
-    server_2.serve_forever()
+    server_2 = socketserver.ThreadingUDPServer((HOST, port), MyStageBHandler)
+    server_2_thread = threading.Thread(target=server_2.serve_forever())
+    server_2_thread.daemon = True
+    server_2_thread.start()
 
 
 def stage_3(tcp_port):
-    server_3 = socketserver.TCPServer((HOST, tcp_port), MyStageCHandler, bind_and_activate=True)
-    server_3.serve_forever()
+    server_3 = socketserver.ThreadingTCPServer((HOST, tcp_port), MyStageCHandler, bind_and_activate=True)
+    server3_thread = threading.Thread(target=server_3.serve_forever())
+    server3_thread.daemon = True
+    server3_thread.start()
 
 
 if __name__ == "__main__":
-
-    HOST, PORT = "localhost", 12235
-    server = socketserver.UDPServer((HOST, PORT), MyStageAHandler)
-    server.serve_forever()
+    HOST, PORT = 'localhost', 12235
+    server = socketserver.ThreadingUDPServer((HOST, PORT), MyStageAHandler)
+    server_thread = threading.Thread(target=server.serve_forever())
+    server_thread.daemon = True
+    server_thread.start()
